8c4c6b131d93a83f4a1b2c22768d9e1a9fd3ab3b
Shankharishi Khan <81064200+shankha7139@users.noreply.github.com>
2023-01-27 21:56:03 +0530

    Add files via upload

--- handtrack.py
+++ handtrack.py
@@ -2,11 +2,11 @@ import cv2
 import mediapipe as mp
 import time
 cam=cv2.VideoCapture(1)
-
 mpHands= mp.solutions.hands
 hands = mpHands.Hands()
 mpdarw=mp.solutions.drawing_utils
-
+dict={}
+t=[]
 pt=ct=0
 while True:
     success,img=cam.read()
@@ -21,6 +21,37 @@ while True:
                 cx,cy=int(lm.x*w),int(lm.y*h)
                 if id==0:
                     cv2.circle(img,(cx,cy),25,(255,0,255),cv2.FILLED)
+                if id==4 or id ==8 or id==12 or id==16 or id ==20:
+                    cv2.circle(img,(cx,cy),15,(0,0,255),cv2.FILLED)
+
+                if id==0:
+                    p=cx
+                    q=cy
+                if id==8 or id==12 or id==16 or id==20:
+                    if p>cx :
+                        cv2.circle(img,(cx,cy),15,(0,255,0),cv2.FILLED)
+                        t.append('green')
+                    else:
+                        t.append('red')
+                    
+            print(set(t))
+            if set(t)=={'green'}:
+                for id,lm in enumerate(handlms.landmark):
+                    h,w,c=img.shape
+                    cx,cy=int(lm.x*w),int(lm.y*h)
+                    if id==0:
+                        cv2.circle(img,(cx,cy),25,(0,255,0),cv2.FILLED)
+                        cv2.putText(img,'RIGHT',(500,70),cv2.FONT_HERSHEY_PLAIN,3,(255,0,255),2)
+            if set(t)=={'red'}:
+                for id,lm in enumerate(handlms.landmark):
+                    h,w,c=img.shape
+                    cx,cy=int(lm.x*w),int(lm.y*h)
+                    if id==0:
+                        cv2.circle(img,(cx,cy),25,(0,0,255),cv2.FILLED)
+                        cv2.putText(img,'LEFT',(500,70),cv2.FONT_HERSHEY_PLAIN,3,(255,0,255),2)
+
+            t=[]
+   
             mpdarw.draw_landmarks(img,handlms,mpHands.HAND_CONNECTIONS)
 
     ct=time.time()
